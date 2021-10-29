aa1421bfed8159cb67847bbf108a7178414f6498
Ivana <79462114+IvanaWinc@users.noreply.github.com>
2021-10-29 06:39:40 +0200

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,27 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+  
+  ikRockArrowFunctions()
+  
+  const ikRockArrowFunctions =  () => console.log("Joe, ik rock de arrow functions!");;
+  
+  ikRockArrowFunctions()
+
+  const fivePlusSeven = function () {
+    return 5 + 7
+  };
+  
+  fivePlusSeven();
+
+  const fivePlusSeven = () => 5 + 7;
+  
+ fivePlusSeven();
+
+let myFunction = (a, b) => a + b
+
+let addFive = a => a + 5;
+
+let createObject = () => cosole.log ("hoi");
+  
